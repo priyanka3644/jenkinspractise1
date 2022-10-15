b6b2cb636103cd5cfce9cb76df02d5ebd0a65e2f
priyanka <priyankasanka@gmail.com>
2022-10-15 20:46:46 +0530

    initialcommit

---
+++ Tests/test_userRegistration.py
@@ -0,0 +1,18 @@
+import pytest
+
+class Test_Registration():
+    def test_registration(self):
+        print("inside user registration")
+
+    def test_login(self):
+        print("inside user login test")
+
+    def test_logout(self):
+        print("inside logout test")
+
+class Test_AddProducts():
+    def test_addproducts(self):
+        print("inside add products test")
+
+    def test_checkout(self):
+        print("inside checkout test")
